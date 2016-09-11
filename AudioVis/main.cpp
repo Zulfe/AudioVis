c75f9d057a8d2862722623a933fbd4f2f2504a99
Damon <Zulfe@live.com>
2016-09-11 00:06:19 -0400

    started

---
+++ AudioVis/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Damon
+ *
+ * Created on September 11, 2016, 12:00 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Chris sucks!" << endl;
+    return 0;
+}
+
